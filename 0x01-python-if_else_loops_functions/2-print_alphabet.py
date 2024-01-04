d18cbc3ad2c8fea25edc05b51c939ac41f505ab3
codefox254 <frankode254@gmail.com>
2024-01-04 01:13:18 -0800

    adding new changes

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
-for c in range(ord('a'), ord('z') + 1):
-    print("{}".format(chr(c)), end="")
+# Loop from 97 to 122, which are the ASCII codes for lowercase letters
+for i in range(97, 123):
+# Print the character corresponding to the ASCII code, without a new line
+print("{}".format(chr(i)), end="")
+# Print a new line at the end
 print()
