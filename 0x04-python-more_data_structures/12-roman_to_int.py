f0737012cb98259f3de6d58b0b8d778cee7174fc
codefox254 <frankode254@gmail.com>
2024-01-11 06:50:07 -0800

    amending 12-roman

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,21 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    # Check if the input is valid
     if roman_string is None or type(roman_string) != str:
         return 0
-    # Define a dictionary of Roman symbols and values
     data = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    # Initialize the result as zero
     result = 0
-    # Loop through the characters of the Roman numeral
     for i in range(len(roman_string)):
-        # Get the value of the current symbol
         value = data[roman_string[i]]
-        # If the next symbol exists and is larger than the current one, subtract the value
         if i < len(roman_string) - 1 and data[roman_string[i + 1]] > value:
             result -= value
-        # Otherwise, add the value
         else:
             result += value
-    # Return the result
     return result
