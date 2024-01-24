a5ef141c4a90b4e2caa2c58460debf415f416a83
codefox254 <frankode254@gmail.com>
2024-01-24 06:40:41 -0800

    adding task 3

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""the Square module."""
+
+
+class Square:
+    """Defines the square."""
+
+    def __init__(self, size):
+        """Constructors.
+        Args:
+            size: the length of side of the square.
+        """
+        self.__size = size
