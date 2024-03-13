b60f94a38e8174ffd28573b61a2311dd8678e116
codefox254 <91884653+codefox254@users.noreply.github.com>
2024-03-13 20:58:54 +0300

    Update 100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -3,7 +3,5 @@ The cript that converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_uni
 */
 -- Convert database to UTF8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- Convert table to UTF8
 ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- Convert field to UTF8
 ALTER TABLE hbtn_0c_0.first_table MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
