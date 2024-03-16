128bb84a1a91b420d752e46d8bb730483ef5a180
codefox254 <91884653+codefox254@users.noreply.github.com>
2024-03-16 21:09:16 +0300

    Update 10-top_score.sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,8 +1,4 @@
-/*
-Task:
-A script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
-*/
 -- Select all records from second_table, displaying score and name, ordered by score (top first)
 SELECT score, name
-FROM hbtn_0c_0.second_table
+FROM second_table
 ORDER BY score DESC;
