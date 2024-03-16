8072fe72ae58a2f7cc77d533ac41734d499f708b
codefox254 <91884653+codefox254@users.noreply.github.com>
2024-03-16 20:24:48 +0300

    Updating 5-full_table.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,6 @@
--- Retrieve table structure without using DESCRIBE or EXPLAIN
+-- Retrieve the structure of a table named 'first_table' in the 'hbtn_0c_0' database
+-- by selecting column name, type, nullability, key, default value, and extra information
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM information_schema.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' -- Change 'hbtn_0c_0' to the desired database name
-    AND TABLE_NAME = 'first_table'; -- Change 'first_table' to the desired table name
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' -- Specify the desired database name
+    AND TABLE_NAME = 'first_table'; -- Specify the desired table name
