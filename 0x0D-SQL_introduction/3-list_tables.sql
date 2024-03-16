dbfeb8c7b2daf91367ab9ccba6b02a1de79add90
codefox254 <91884653+codefox254@users.noreply.github.com>
2024-03-16 19:53:32 +0300

    Update comments on 3-list_tables.sql

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,4 +1,4 @@
--- List all tables in the specified database
-SELECT TABLE_NAME
-FROM information_schema.TABLES
-WHERE TABLE_SCHEMA = DATABASE();
+-- List all tables in the current database
+SELECT TABLE_NAME AS Tables_in_hbtn_test_db_0 -- Select the table names and alias the column
+FROM information_schema.TABLES -- From the information schema's tables
+WHERE TABLE_SCHEMA = DATABASE(); -- Where the table schema matches the current database
