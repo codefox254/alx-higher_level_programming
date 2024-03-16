7d308f2f610ef512c3a088376e3f115bfa7648d4
codefox254 <91884653+codefox254@users.noreply.github.com>
2024-03-16 20:45:02 +0300

    Update 6-list_values.sql

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,3 +1,2 @@
 -- Select all rows from the first_table
-SELECT *
-FROM hbtn_0c_0.first_table; -- Change 'hbtn_0c_0' and 'first_table' to the desired database and table names
+SELECT * FROM hbtn_0c_0.first_table;
