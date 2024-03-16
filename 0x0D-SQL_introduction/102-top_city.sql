c068ace0101d254230e62dccd7537e8a3642a01b
codefox254 <91884653+codefox254@users.noreply.github.com>
2024-03-16 21:36:52 +0300

    Update 102-top_city.sql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,12 +1,6 @@
-/*
-Task:
-Write a script that displays the top 3 cities' temperatures during July and August ordered by temperature (descending).
-*/
-
 -- Select top 3 cities' temperatures during July and August, ordered by temperature (descending)
 SELECT city, temperature
 FROM hbtn_0c_0.temperature_data
 WHERE MONTH(date) IN (7, 8)  -- Filter for July and August
 ORDER BY temperature DESC
 LIMIT 3;  -- Limit to top 3 results
-
