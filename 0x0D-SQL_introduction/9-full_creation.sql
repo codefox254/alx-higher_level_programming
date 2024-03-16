b99eae79053c32ddd1604c73b1ba08fb2856f47a
codefox254 <91884653+codefox254@users.noreply.github.com>
2024-03-16 20:40:46 +0300

    Update 9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,14 +1,13 @@
--- Create table if not exists
-CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
-    id INT,
-    name VARCHAR(256),
-    score INT
+-- Create a table named 'second_table' with columns 'id', 'name', and 'score'
+-- if it doesn't already exist, and fill it with multiple rows
+CREATE TABLE IF NOT EXISTS `second_table` (
+    `id` INT,
+    `name` VARCHAR(256),
+    `score` INT
 );
 
--- Insert multiple rows into second_table
-INSERT INTO hbtn_0c_0.second_table (id, name, score)
-VALUES
-    (1, 'John', 10),
-    (2, 'Alex', 3),
-    (3, 'Bob', 14),
-    (4, 'George', 8);
+-- Insert rows into the 'second_table' with specified values for id, name, and score
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, 'John', 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, 'Alex', 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, 'Bob', 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, 'George', 8);
