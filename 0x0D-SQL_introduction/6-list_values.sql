3be0f8096da32e05547d4b7c222e2d8c9c348d44
codefox254 <91884653+codefox254@users.noreply.github.com>
2024-03-16 20:27:01 +0300

    Update 6-list_values.sql

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,11 +1,3 @@
-/*
-Task:
-Write a script that lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server.
-All fields should be printed.
-The database name will be passed as an argument of the mysql command.
-*/
-
 -- Select all rows from the first_table
 SELECT *
-FROM hbtn_0c_0.first_table; -- Change 'hbtn_0c_0' and 'first_table' to the desired database and table names, respectively
-
+FROM hbtn_0c_0.first_table; -- Change 'hbtn_0c_0' and 'first_table' to the desired database and table names
