35049fa07397b2adf28e42d90b3f2106b06510cf
codefox254 <91884653+codefox254@users.noreply.github.com>
2024-03-30 00:54:08 +0300

    Update 5-unique_id.sql

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,3 @@
 -- SQL Script: Create table unique_id in the hbtn_0d_2 database if not exists
 -- Attempt to create the table unique_id in the hbtn_0d_2 database if it doesn't exist
-REATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
