d85302153a57e52f72d45924c3ddb0c10e826a5d
codefox254 <91884653+codefox254@users.noreply.github.com>
2024-03-30 01:09:53 +0300

    Update 12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,6 @@
 --This will list all shows contained in hbtn_0d_tvshows without a genre linked
 -- This aalso lists all rows of a database that don't have one column
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON id=tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY title, tv_show_genres.genre_id;
