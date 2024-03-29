2d23f605e4fb19d5c9f4f4e9ed54602967045e10
codefox254 <91884653+codefox254@users.noreply.github.com>
2024-03-30 00:55:35 +0300

    Update 6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,7 @@
 -- SQL Script: Create database hbtn_0d_usa and table states if not exists
 -- Check if the database hbtn_0d_usa already exists by attempting to create it
-CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
+REATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- uses a database
+USE hbtn_0d_usa;
+-- Will create a table
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
