ef4e7efd012cca3f4b2272878cd8d597531de855
frank <frankode254@gmail.com>
2024-04-08 22:15:37 +0300

    adding the first tasks

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+// Constant variable declaration
+const myVar = "JavaScript is amazing";
+
+// Printing output
+console.log(myVar);
---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// Get the first argument passed to the script
+const arg = process.argv[2];
+
+// Check if an argument is passed and print the message accordingly
+if (arg === undefined) {
+    console.log("No argument");
+} else {
+    console.log(arg);
+}
