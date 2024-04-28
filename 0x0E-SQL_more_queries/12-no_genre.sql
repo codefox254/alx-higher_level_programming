a5f519a560ba82e865e78a679053f9dbe8c5faab
frank <frankode254@gmail.com>
2024-04-28 13:53:31 +0300

    adding done

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,4 +1,3 @@
---This will list all shows contained in hbtn_0d_tvshows without a genre linked
 -- This aalso lists all rows of a database that don't have one column
 SELECT title, tv_show_genres.genre_id FROM tv_shows
 LEFT JOIN tv_show_genres ON id=tv_show_genres.show_id
