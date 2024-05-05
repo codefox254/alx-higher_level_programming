7e891fc878bb3530415706876ad827988ebd2388
codefox254 <91884653+codefox254@users.noreply.github.com>
2024-05-05 13:19:36 +0300

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a GET request to the URL, and displays the body of the response if the status code is 200
-response=$(curl -s -o /tmp/response.txt -w "%{http_code}" $1); status_code=$(tail -c 3 <<< $response); if [ $status_code -eq 200 ]; then cat /tmp/response.txt; fi
+# This script sends a GET request to a URL and displays the body of the response if the status
+[ "$status_code" -eq 200 ] && curl -s $1
