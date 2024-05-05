7e45141f91d19453e798cf0ed12ab2c438c64fc8
codefox254 <91884653+codefox254@users.noreply.github.com>
2024-05-05 12:56:35 +0300

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script makes a request to 0.0.0.0:5000/catch_me
-curl -s -X GET 0.0.0.0:5000/catch_me
+curl -s -X CORRECT_METHOD -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
