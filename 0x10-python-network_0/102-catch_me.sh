ff45e06ec5126ffa69d631274c1fcd6d7154f4d2
codefox254 <91884653+codefox254@users.noreply.github.com>
2024-05-05 13:02:08 +0300

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # This script makes a request to 0.0.0.0:5000/catch_me
-response=$(curl -sL -X GET 0.0.0.0:5000/catch_me)
-if [[ "$response" == *"You find me!"* ]]; then
-    echo "Success! The server responded with the expected message."
-else
-    echo "Unexpected response from the server."
-fi
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me | grep 'You got me!'
