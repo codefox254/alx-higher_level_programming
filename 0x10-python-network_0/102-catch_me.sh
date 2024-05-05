ada84bdd6f3ebae207346254df775c98cada3a62
codefox254 <91884653+codefox254@users.noreply.github.com>
2024-05-05 13:03:49 +0300

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# This script makes a request to 0.0.0.0:5000/catch_me
-curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me | grep 'You got me!'
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me | grep 'You find me!'
