a64763aa89ffefbd3fafee818695cd52576b11db
codefox254 <91884653+codefox254@users.noreply.github.com>
2024-05-05 13:06:58 +0300

    Update 100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a request to a URL passed as an argument and displays only the status code of the response
-curl -s -o /tmp/response.txt $1; status_code=$(grep -oE '[0-9]{3}' /tmp/response.txt); rm /tmp/response.txt; echo $status_code
+status_code=$(curl -s -o /dev/null -w "%{http_code}" "$1")echo "$status_code"
