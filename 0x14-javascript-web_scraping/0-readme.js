494bd81ddc03ca4dbef84ce77e604fd70e4f7688
codefox254 <91884653+codefox254@users.noreply.github.com>
2024-05-23 03:40:37 +0300

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,11 +2,12 @@
 //add file
 
 const fs = require('fs');
-const filePath = process.argv[2];
-fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
+const file = process.argv[2];
+
+fs.readFile(file, 'utf-8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
     console.log(data);
+  }
 });
