5ffd6533e2639e4f19ebb72c7fdf2a02314d0768
codefox254 <91884653+codefox254@users.noreply.github.com>
2024-05-23 04:19:55 +0300

    Update 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-//prints all characters of a Star Wars movie
+// Prints all characters of a Star Wars movie
 
 const request = require('request');
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
 request.get(url, (error, response, body) => {
   if (error) {
     console.log(error);
