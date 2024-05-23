31a574ebfd81851e60714ff53c67ea6c5563236e
codefox254 <91884653+codefox254@users.noreply.github.com>
2024-05-23 04:18:48 +0300

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 
 const fs = require('fs');
+
 fs.readFile(process.argv[2], 'utf8', function (error, content) {
-if (error) {
+  if (error) {
     console.error('Error reading the file:', error);
   } else {
-    // If the file is read successfully, the 'content' parameter will contain the contents of the file as a string.
+    // If the file is read successfully, the 'content' parameter.
     console.log(content);
   }
 });
